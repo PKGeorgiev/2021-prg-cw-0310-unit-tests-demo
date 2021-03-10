900b5338c5b287ef1f74ef43682ed20865ace3bf
SMPonchev18 <SMPonchev18@codingburgas.bg>
2021-03-10 09:58:06 +0200

    Comit with FalseArgument

--- UnitTests_SMPonchev18/UnitTests_SMPonchev18.cpp
+++ UnitTests_SMPonchev18/UnitTests_SMPonchev18.cpp
@@ -8,11 +8,38 @@ namespace UnitTestsSMPonchev18
 	TEST_CLASS(UnitTestsSMPonchev18)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+			// 14 => true
+			// 8  => false
+			// 10 => false
+			// 20 => true
+			// 21 =>false
+		TEST_METHOD(ShouldReturnTrueIfArgumentIsInRange)
 		{
+			// AAA
+
+			// Arrange
 			bool result;
-			result=isNumberInRange(17);
+			int argument = 14;
+
+			// Act
+			result=isNumberInRange(14);
+
+			// Assert
+			Assert::AreEqual(true, result);
+		}
+		TEST_METHOD(ShouldReturnFalseIfArgumentIsOutOfRange)
+		{
+			// AAA
+
+			// Arrange
+			bool result;
+			int argument = 8;
+
+			// Act
+			result = isNumberInRange(argument);
+
+			// Assert
+			Assert::AreEqual(false, result);
 		}
 	};
 }
