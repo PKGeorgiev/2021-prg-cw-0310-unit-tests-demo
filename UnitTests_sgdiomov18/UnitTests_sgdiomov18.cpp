7f2a124ee15688b6e107d990d3ebd293524861c1
SGDiomov18 <56921460+SGDiomov18@users.noreply.github.com>
2021-03-10 16:52:44 +0200

    Add Unit Test project

---
+++ UnitTests_sgdiomov18/UnitTests_sgdiomov18.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../number-tools/numberTools.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestssgdiomov18
+{
+	TEST_CLASS(UnitTestssgdiomov18)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int result = isNumberInRange(17);
+		}
+	};
+}
