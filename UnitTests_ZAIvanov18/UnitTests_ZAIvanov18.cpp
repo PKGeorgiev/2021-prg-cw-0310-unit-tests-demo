db05867d3aaa122c5c990993af8585332fbb19ee
ZAIvanov18 <58166571+ZAIvanov18@users.noreply.github.com>
2021-03-10 09:35:49 +0200

    Create UnitTest project and configure dependencies.

---
+++ UnitTests_ZAIvanov18/UnitTests_ZAIvanov18.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../number-tools/numberTools.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestsZAIvanov18
+{
+	TEST_CLASS(UnitTestsZAIvanov18)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
